a4c3fde509b8b8faa55ed07fb8c2557192269b75
Ketchuskana <105216628+Ketchuskana@users.noreply.github.com>
2026-01-20 15:29:43 +0100

    convertir csv gold en parquets

--- flows/convertToParquet.py
+++ flows/convertToParquet.py
@@ -7,6 +7,6 @@ gold_path = pathlib.Path("data/gold")
 # pour chaque CSV dans le dossier
 for csv_file in gold_path.glob("*.csv"):
     df = pd.read_csv(csv_file)
-    parquet_file = csv_file.with_suffix(".parquet")  # change .csv en .parquet
+    parquet_file = csv_file.with_suffix(".parquet") 
     df.to_parquet(parquet_file, index=False)
     print(f"Converti {csv_file.name} → {parquet_file.name}")
